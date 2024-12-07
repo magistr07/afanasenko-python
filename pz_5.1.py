93a44572108ed74ef807864b7fe78b5953482639
magistr07 <egorafnasenko@gmail.com>
2024-12-07 12:12:29 +0300

    Update pz_5.1.py

--- pz_5.1.py
+++ pz_5.1.py
@@ -1,13 +1,19 @@
-a = input()
-b = list(a)
-print(b)
+import random
+
+def print_forty_random_characters():
+    
+    characters = (
+        'abcdefghijklmnopqrstuvwxyz'
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        '0123456789'
+        '!@#$%^&*()_+-=[]{}|;:,.<>?'
+    )
+    
+    
+    random_characters = ''.join(random.choice(characters) for _ in range(40))
+    
+   
+    print(random_characters)
 
 
-def summ(b):
-    f = 0
-    for i in range(len(b)):
-        f = f + int(b[i])
-    print(f)
-
-
-summ(b)
+print_forty_random_characters()
