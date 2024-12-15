3284e96c7b65f3c05439223ca0d6b11d59ef00f2
magistr07 <egorafnasenko@gmail.com>
2024-12-15 21:45:28 +0300

    Update pz_5.1.py

--- pz_5.1.py
+++ pz_5.1.py
@@ -4,23 +4,14 @@
 '''
 
 
-
 import random
-
 def print_forty_random_characters():
-    
     characters = (
         'abcdefghijklmnopqrstuvwxyz'
         'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
         '0123456789'
         '!@#$%^&*()_+-=[]{}|;:,.<>?'
     )
-    
-    
     random_characters = ''.join(random.choice(characters) for _ in range(40))
-    
-   
     print(random_characters)
-
-
 print_forty_random_characters()
