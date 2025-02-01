a703282835a4338ecc588af841f2530f415370e8
magistr07 <egorafnasenko@gmail.com>
2025-02-01 15:50:16 +0300

    Update pz_6.1.py

--- pz_6.1.py
+++ pz_6.1.py
@@ -1,1 +1,9 @@
+# Создаем пустой список
+powers_of_two = []
 
+# Заполняем список степенями двойки от 1 до 10
+for i in range(1, 11):
+    powers_of_two.append(2 ** i)
+
+# Выводим результат
+print(powers_of_two)
