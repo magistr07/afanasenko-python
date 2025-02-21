2ab90d188bcdca9f68ba085125740903e71acc29
magistr07 <egorafnasenko@gmail.com>
2025-02-21 17:01:48 +0300

    Create pz_9.py

---
+++ pz_9.py
@@ -0,0 +1,20 @@
+data = "Петров Иван ПОКС-29 5 4 3 2 5 4 4 5 4"
+
+parts = data.split()
+
+surname = parts[0]
+name = parts[1]
+group = parts[2]
+grades = list(map(int, parts[3:]))
+
+average_grade = sum(grades) / len(grades)
+
+result = {
+    "Фамилия": surname,
+    "Имя": name,
+    "Группа": group,
+    "Оценки": grades,
+    "Средний балл": average_grade
+}
+
+print(result)
